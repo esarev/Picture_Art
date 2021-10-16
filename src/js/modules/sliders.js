3d420e46ecedfebbb85d8c68ade758895b359aea
Alex <esarev@inbox.ru>
2021-10-16 19:53:42 +0700

    slides

---
+++ src/js/modules/sliders.js
@@ -0,0 +1,27 @@
+const sliders = (slides, prev, dir, next) =>  {
+  let slideIndex = 1;
+  const items = document.querySelectorAll(slides),
+        prevBtn = document.querySelector(prev),
+        nextBtn = document.querySelector(next);
+
+  function showSlides(n) {
+    if(n > items.length) {
+      slideIndex = 1;
+    }
+
+    if(n < 1) {
+      slideIndex = items.length;
+    }
+
+    items.forEach(item => {
+      item.classList.add('animated');
+      item.style.display = 'none';
+    });
+
+    items[slideIndex - 1].style.display = 'block';
+  }
+  
+  showSlides(slideIndex);
+};
+
+export default sliders;
